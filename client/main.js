52391a45b7217d8acf8f006faf24b6cf7d246d06
guykanbar <guykanbar@gmail.com>
2023-07-27 10:16:51 +0200

    remove auto-publish

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
 import './main.html';
 import './export/export-list.html'
 import './export/export-list'
+
+Meteor.subscribe('exports')
--- server/main.js
+++ server/main.js
@@ -1,7 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import './usecase/export-list';
 import './usecase/exports-delete'
+import { ExportsCollection } from './collections/export';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.publish('exports', () => {
+  return ExportsCollection.find()
+})
